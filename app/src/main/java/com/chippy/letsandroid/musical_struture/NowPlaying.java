f2f631b7d2e203c9380db0b33062ab71272a7baa
abhishek999jain <abhishek999jain@gmail.com>
2016-12-09 12:44:59 +0530

    Initial commit

---
+++ app/src/main/java/com/chippy/letsandroid/musical_struture/NowPlaying.java
@@ -0,0 +1,11 @@
+package com.chippy.letsandroid.musical_struture;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+public class NowPlaying extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_now_playing);
+    }
+}
